f8c97d126e461ce9d8fd58ea6eeab6b54a896f7f
Rahul S <rahul.suresh307@gmail.com>
2022-07-05 22:47:35 +0530

    first commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import Link from 'next/link';
+import {AiOutlineShopping} from 'react-icons/ai'
+
+const Navbar = () => {
+  return (
+    <div className='navbar-container'>
+      <p className="logo">
+        <Link href="/">Rahul's Store</Link>
+      </p>
+
+      <button className='cart-icon' type='button' onClick="">
+        <AiOutlineShopping />
+        <span className='cart-item-qty'>1</span>
+      </button>
+    </div>
+  )
+}
+
+export default Navbar
